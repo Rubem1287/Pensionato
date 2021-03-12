7a6a267675935b5ac6c6be7f448eb385455b1cd5
Rubem Silva <rubem.edu@outlook.com>
2021-03-11 21:28:11 -0300

    Adicionar arquivos de projeto.

---
+++ Pensionato/Estudantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensionato
+{
+    class Estudantes
+    {
+        public int Quarto { get; private set; }
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+
+       
+        public Estudantes(int quarto, string nome, string email)
+        {
+            
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+    }
+}
---
+++ Pensionato/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] estudantes = new Estudantes[10];
+
+            Console.Write("Digite o numero de estudantes que vão alugar os quartos : ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n > 10 || n < 0)
+            {
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Registro de estudantes : ");
+            for (int p = 0; p < n; p++)
+            {
+                Console.Write("Nome : ");
+                string nome = Console.ReadLine();
+                Console.Write("Email : ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto : ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                estudantes[p] = new Estudantes(quarto, email, nome);
+                
+
+            }
+
+            Console.Write("Deseja imprimir um relatório de ocupações do pensionato? (s/n) ");
+            char r = char.Parse(Console.ReadLine());
+
+            int count = 0;
+            if (r == 's' || r == 'S')
+            {
+                for (int p = 0; p < 10; p++)
+                {
+                   
+                    if (estudantes[p].Nome != null)
+                    {
+                        count++;
+                        Console.WriteLine("Registro de aluguel #" + count + ":");
+                        Console.WriteLine("Nome : " + estudantes[p].Nome);
+                        Console.WriteLine("Email : " + estudantes[p].Email);
+                        Console.WriteLine("Quarto : " + estudantes[p].Quarto);
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else if(r == 'n' || r == 'N')
+            {
+                Console.WriteLine("Obrigado por usar o programa...");
+            }
+
+
+
+        }
+    }
+}
